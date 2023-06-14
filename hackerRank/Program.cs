463170d699b5ae2ecfbada75e4e3dcc7aab3af4c
Alessio M <alessio.marziali@gmail.com>
2023-06-14 15:53:06 +0100

    GradingStudents (easy) and Time in words challenges (medium)

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -11,18 +11,214 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace hackerRank
 {
     class Program
     {
-
+        
         /*
-         * Complete the 'birthdayCakeCandles' function below.
+         * Complete the 'timeInWords' function below.
          *
-         * The function is expected to return an INTEGER.
-         * The function accepts INTEGER_ARRAY candles as parameter.
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. INTEGER h
+         *  2. INTEGER m
          */
+        public static string timeInWords(int h, int m)
+        {
+            Dictionary<int, string> hours = new Dictionary<int, string>();
+            hours.Add(1, "one");
+            hours.Add(2, "two");
+            hours.Add(3, "three");
+            hours.Add(4, "four");
+            hours.Add(5, "five");
+            hours.Add(6, "six");
+            hours.Add(7, "seven");
+            hours.Add(8, "eight");
+            hours.Add(9, "nice");
+            hours.Add(10, "ten");
+            hours.Add(11, "eleven");
+            hours.Add(12, "twelve");
+
+            Dictionary<int, string> minutes = new Dictionary<int, string>();
+            minutes.Add(0, "o'clock");
+            minutes.Add(1, "one");
+            minutes.Add(2, "two");
+            minutes.Add(3, "three");
+            minutes.Add(4, "four");
+            minutes.Add(5, "five");
+            minutes.Add(6, "six");
+            minutes.Add(7, "seven");
+            minutes.Add(8, "eight");
+            minutes.Add(9, "nine");
+            minutes.Add(10, "ten");
+            minutes.Add(11, "eleven");
+            minutes.Add(12, "twelve");
+            minutes.Add(13, "thirteen");
+            minutes.Add(14, "fourteen");
+            minutes.Add(15, "quarter");
+            minutes.Add(16, "sixteen");
+            minutes.Add(17, "seventeen");
+            minutes.Add(18, "eighteen");
+            minutes.Add(19, "nineteen");
+            minutes.Add(20, "twenty");
+            minutes.Add(21, "twenty one");
+            minutes.Add(22, "twenty two");
+            minutes.Add(23, "twenty three");
+            minutes.Add(24, "twenty four");
+            minutes.Add(25, "twenty five");
+            minutes.Add(26, "twenty six");
+            minutes.Add(27, "twenty seven");
+            minutes.Add(28, "twenty eight");
+            minutes.Add(29, "twenty nine");
+            minutes.Add(30, "thirty");
+            minutes.Add(31, "thirty one");
+            minutes.Add(32, "thirty two");
+            minutes.Add(33, "thirty three");
+            minutes.Add(34, "thirty four");
+            minutes.Add(35, "thirty five");
+            minutes.Add(36, "thirty six");
+            minutes.Add(37, "thirty seven");
+            minutes.Add(38, "thirty eight");
+            minutes.Add(39, "thirty nine");
+            minutes.Add(40, "forty");
+            minutes.Add(41, "forty one");
+            minutes.Add(42, "forty two");
+            minutes.Add(43, "forty three");
+            minutes.Add(44, "forty four");
+            minutes.Add(45, "forty five");
+            minutes.Add(46, "forty six");
+            minutes.Add(47, "forty seven");
+            minutes.Add(48, "forty eight");
+            minutes.Add(49, "forty nine");
+            minutes.Add(50, "fifty");
+            minutes.Add(51, "fifty one");
+            minutes.Add(52, "fifty two");
+            minutes.Add(53, "fifty three");
+            minutes.Add(54, "fifty four");
+            minutes.Add(55, "fifty five");
+            minutes.Add(56, "fifty six");
+            minutes.Add(57, "fifty seven");
+            minutes.Add(58, "fifty eight");
+            minutes.Add(59, "fifty nine");
+
+            string currentHour = hours[h].ToString();
+
+            string returnValue = string.Empty;
+            // This could be done much better
+            switch (m)
+            {
+                case 1:
+                    returnValue = "one minute past " + currentHour;
+                    break;
+                case 15:
+                    returnValue = minutes[m] + " past " + hours[h];
+                    break;
+                case > 1:
+                    if (m < 30)
+                    {
+                        returnValue = minutes[m] + " minutes past " + hours[h];    
+                    }
+                    else
+                    {
+                        if (m == 30)
+                        {
+                            returnValue = "half past " + currentHour;
+                        }
+                        else
+                        {
+                            int difference = 60 - m;
+                            if (difference == 15)
+                            {
+                                returnValue = minutes[difference] + " to " + (hours[h+1]);
+                            }
+                            else
+                            {
+                                returnValue = minutes[difference] + " minutes to " + (hours[h+1]); 
+                            }
+                        }
+                    }
+                    break;
+               
+                case 0:
+                    returnValue = currentHour + " o' clock";
+                    break;
+            }
+            return returnValue;
+        }
+        
+        /*
+         * Complete the 'gradingStudents' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY grades as parameter.
+         */
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            List<int> output = new List<int>();
+            
+            foreach (int i in grades)
+            {
+                // If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+                if (i < 38)
+                {
+                    output.Add(i);
+                }
+                else
+                {
+                    int tempScore = i;
+                    int counter = 0;
+                    int maxCounter = 2;
+
+                    if (i % 5 == 0)
+                    {
+                        output.Add(i);
+                    }
+                    else
+                    {
+                        while (tempScore % 5 != 0)
+                        {
+                            tempScore++;
+                            counter++;
+                        }
+
+                        if (counter <= maxCounter && tempScore % 5 == 0)
+                        {
+                            output.Add(tempScore);
+                        }
+                        else
+                        {
+                            output.Add(i);
+                        }
+                    }
+     
+                }
+            }
+            return output;
+        }
+        
+        
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            List<int> finalResponse = new List<int>();
+            var uniqueRanked = ranked.Distinct().ToList();
+            int n = uniqueRanked.Count;
+            int j = n - 1;
+
+            foreach (int score in player)
+            {
+                while (j >= 0 && score >= uniqueRanked[j])
+                {
+                    j--;
+                }
+
+                finalResponse.Add(j + 2);
+            }
+            return finalResponse;
+        }
+
         public static int birthdayCakeCandles(List<int> candles)
         {
             var maxHeight = candles.Max();
@@ -30,11 +226,6 @@ namespace hackerRank
             return counter;
         }
         
-        /*
-         * Complete the 'miniMaxSum' function below.
-         *
-         * The function accepts INTEGER_ARRAY arr as parameter.
-         */
         public static void miniMaxSum(List<int> arr)
         {
             arr.Sort();
@@ -49,12 +240,6 @@ namespace hackerRank
             Console.WriteLine(totalLowest+ " " + totalHighest);
         }
         
-        /*
-         * Complete the 'timeConversion' function below.
-         *
-         * The function is expected to return a STRING.
-         * The function accepts STRING s as parameter.
-         */
         public static string timeConversion(string s)
         {
             var splitSArray = s.Split(":");
@@ -82,11 +267,6 @@ namespace hackerRank
             return test;
         }
         
-        /*
-        * Complete the 'staircase' function below.
-        *
-        * The function accepts INTEGER n as parameter.
-        */
         public static void staircase(int n)
         {
 
@@ -133,12 +313,6 @@ namespace hackerRank
             }
         }
         
-        /*
-         * Complete the 'diagonalDifference' function below.
-         *
-         * The function is expected to return an INTEGER.
-         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-         */
         public static int diagonalDifference(List<List<int>> arr)
         {
             int rowsColumns = arr.Count;
@@ -182,12 +356,6 @@ namespace hackerRank
             return Math.Abs(test);
         }
         
-        /*
-         * Complete the 'aVeryBigSum' function below.
-         *
-         * The function is expected to return a LONG_INTEGER.
-         * The function accepts LONG_INTEGER_ARRAY ar as parameter.
-         */
         public static long aVeryBigSum(List<long> ar)
         {
             int ArCount = ar.Count;
@@ -201,11 +369,6 @@ namespace hackerRank
 
         }
         
-        /*
-         * Complete the 'plusMinus' function below.
-         *
-         * The function accepts INTEGER_ARRAY arr as parameter.
-         */
         public static void plusMinus(List<int> arr)
         {
             int positives = 0;
@@ -241,16 +404,11 @@ namespace hackerRank
         
         public static void Main()
         {
-            /*
-            List<int> arr = new List<int>();
-            arr.Add(-4);
-            arr.Add(3);
-            arr.Add(-9);
-            arr.Add(0);
-            arr.Add(4);
-            arr.Add(1);
-            plusMinus(arr);
-            */
+            Console.WriteLine(timeInWords(4, 15));
+            
+
+
+
         }
     }
 }
